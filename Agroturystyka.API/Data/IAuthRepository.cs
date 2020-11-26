9b9c09114823093d5dcbad290230138aa090e51e
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-11-26 17:14:44 +0100

    utworzenie interfejsu do autoryzacji

---
+++ Agroturystyka.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using Agroturystyka.API.Models;
+using System.Threading.Tasks;
+
+namespace Agroturystyka.API.Data
+{
+    interface IAuthRepository
+    {
+        Task<User> Login(string username, string password);
+        Task<User> Register(User user, string password);
+        Task<bool> UserExists(string username);
+    }
+}
