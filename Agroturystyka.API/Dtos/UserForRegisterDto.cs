4ac2d321c8e3a1329725169f97a260e89e1826af
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-11-27 10:33:01 +0100

    Walidacja rejestracji

--- Agroturystyka.API/Dtos/UserForRegisterDto.cs
+++ Agroturystyka.API/Dtos/UserForRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,15 @@ namespace Agroturystyka.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage ="Nazwa użytkownika jest wymgana")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymgane")]
+        [StringLength(12, MinimumLength =6, ErrorMessage ="Hasło musi się składać od 6 do 12 znaków!")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "E-mail jest wymagany!")]
+        [EmailAddress(ErrorMessage = "Nie porawny adres E-mail!")]
         public string Email { get; set; }
     }
 }
