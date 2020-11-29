5e737c33ade69173a3b4d2f1a372c0b8320a013f
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-11-29 16:13:29 +0100

    Formularz logowania

--- Agroturystyka.API/Controllers/WeatherForecastController.cs
+++ Agroturystyka.API/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Agroturystyka.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -10,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace Agroturystyka.API.Controllers
 {
     //http://localhost:8164/weatherforecast
-
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -28,6 +29,7 @@ namespace Agroturystyka.API.Controllers
             return Ok(weather);
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
