6ca2b0caafcc94091328d8c25dc479c2b2ef4fc4
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-12-01 13:17:00 +0100

    dodanie angular jwt - rozkodowanie tokena

--- Agroturystyka.SPA/src/app/_services/auth.service.ts
+++ Agroturystyka.SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from "@auth0/angular-jwt";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+jwtHelper = new JwtHelperService();
 baseUrl = 'http://localhost:5000/api/auth/';
 
 constructor(private http: HttpClient) { }
@@ -24,6 +27,11 @@ login(model: any): any{
 
 register(model: any): any{
 return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn(): any{
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token); // jesli token nie wygasl
 }
 
 }
