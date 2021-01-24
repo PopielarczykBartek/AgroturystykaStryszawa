5e90c3937a23321162d92c794b8303a449e88bb5
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2021-01-24 17:24:50 +0100

    dodanie komponenu wyswietlanai stron do farm i rooms

--- Agroturystyka.SPA/src/app/farm/farm.component.ts
+++ Agroturystyka.SPA/src/app/farm/farm.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { Photo } from '../models/photo';
+import { PhotoService } from '../_services/photo.service';
 
 @Component({
   selector: 'app-farm',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FarmComponent implements OnInit {
 
-  constructor() { }
+  photos: Photo[];  
+  isReady = false;
+
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private photoService: PhotoService) { }
 
   ngOnInit() {
+      this.photoService.getPhotos(3).then(x => {
+        this.photos = x;
+        this.setGalleryOptions();
+      });
+    }
+    
+  getImages() {
+    const imagesUrls = [];
+    for (let i = 0; i < this.photos.length; i++) {
+      imagesUrls.push({
+        small: this.photos[i].url,
+        medium: this.photos[i].url,
+        big: this.photos[i].url,
+        description: this.photos[i].description
+      });
+    }
+    return imagesUrls;
+  }
+  
+  setGalleryOptions(){
+    this.galleryOptions = [
+      {
+        width: '600px',
+        height: '400px',
+        thumbnailsColumns: 4,
+        arrowPrevIcon: 'fa fa-chevron-left',
+        arrowNextIcon: 'fa fa-chevron-right',
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+      // max-width 800
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+        preview: false
+      }
+    ];
+    this.isReady = true;
+    this.galleryImages = this.getImages();
   }
 
 }
